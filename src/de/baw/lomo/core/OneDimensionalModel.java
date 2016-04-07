7c9766f91cd9928927edd7b80693b3d389591b87
simons <franz.simons@baw.de>
2016-04-07 17:45:29 +0200

    Removed dependency on specific filling types. Many code improvements, refactoring.

--- src/de/baw/lomo/core/OneDimensionalModel.java
+++ src/de/baw/lomo/core/OneDimensionalModel.java
@@ -1,10 +1,9 @@
 package de.baw.lomo.core;
 
 import de.baw.lomo.core.data.Case;
+import de.baw.lomo.core.data.FillingType;
 import de.baw.lomo.core.data.GateFillingType;
 import de.baw.lomo.core.data.Results;
-import de.baw.lomo.core.data.SegmentGateFillingType;
-import de.baw.lomo.core.data.SluiceGateFillingType;
 
 public class OneDimensionalModel implements Model {
   private static final double GRAVITY = 9.81;
@@ -31,18 +30,8 @@ public class OneDimensionalModel implements Model {
   private double kL;
   /** Kammerbreite **/
   private double kB;
-  /** Kanalflaeche: Austrittsflaeche fuer Strahl **/
-  private double aKanal;
-  /** Verlustbeiwert am Kanal **/
-  private double zetaKanal;
-  /** Strahlausbreitung **/
-  private double jetExponent;
-  /** Strahlausbreitung **/
-  private double jetCoefficient;
-  /** Beginn Rückstaueindluss **/
-  private double maxDh;
   /** Füllorgan **/
-  private GateFillingType filling;
+  private FillingType filling;
   /** Aktuelle Zeit **/
   private double time;
   /** Aktueller Zeitschritt **/
@@ -50,11 +39,11 @@ public class OneDimensionalModel implements Model {
   /** Ortsschrittweite **/
   private double dx;
   /** Bisheriges Fuellvolumen **/
-  private double vol;
+  private double chamberVol;
   /** Beta-Beiwerte **/
   private double[] beta;
   /** Zufluss **/
-  private double[] zufluss;
+  private double[] massSource;
   /** Ganz alte Zeitebene **/
   private double[] Q00, A00;
   /** Alte Zeitebene **/
@@ -66,13 +55,14 @@ public class OneDimensionalModel implements Model {
   /** Postprozessing **/
   private double[] v1, h1;
   /** Schiffsquerschnitte **/
-  private double[] A_schiff_node, A_schiff_cell;
+  private double[] aShipNode, aShipCell;
   /** Aktuelle Schuetzoeffnungshoehe **/
   private double[] valveOpening;
   /** Zeitabhaengige Ergebnisse protokollieren **/
-  private double[] Q, hMean, I, F0, F1, F2;
+  private double[] inflow, hMean, I, F0, F1, F2;
   /** Strickler Wert **/
   private double kSt = 100; // Fest verdrahtet
+  private double[] momentumSource;
 
   private void init() {
 
@@ -89,29 +79,15 @@ public class OneDimensionalModel implements Model {
     kL = data.getChamberLength();
     kB = data.getChamberWidth();
 
-    jetExponent = data.getJetExponent();
-    jetCoefficient = data.getJetCoefficient();
-
-    if (data.getFillingType() instanceof GateFillingType) {
-
-      filling = (GateFillingType) data.getFillingType();
-
-    } else {
-
-      throw new IllegalArgumentException(
-          Messages.getString("errUnknownFillingType") + data.getFillingType()); //$NON-NLS-1$
-    }
-
-    aKanal = filling.getCulvertCrossSection();
-    zetaKanal = filling.getCulvertLoss();
-    maxDh = filling.getSubmergenceStart();
+    filling = data.getFillingType();
 
     // *************************************************************************
     // Räumliche Diskretisierung
     // *************************************************************************
 
     beta = new double[nx];
-    zufluss = new double[nx];
+    massSource = new double[nx];
+    momentumSource = new double[nx];
 
     Q00 = new double[nx + 1];
     A00 = new double[nx];
@@ -128,8 +104,8 @@ public class OneDimensionalModel implements Model {
     v1 = new double[nx + 1];
     h1 = new double[nx];
 
-    A_schiff_node = new double[nx + 1];
-    A_schiff_cell = new double[nx];
+    aShipNode = new double[nx + 1];
+    aShipCell = new double[nx];
 
     // Ortsschrittweite aus Kammerlänge und Knotenanzahl:
     dx = kL / nx;
@@ -140,7 +116,7 @@ public class OneDimensionalModel implements Model {
 
     step = 0;
     time = 0.;
-    vol = 0.;
+    chamberVol = 0.;
 
     // Zeitschrittweite aus Wellengeschwindigkeit und CFL:
     dt = dx / Math.sqrt(GRAVITY * ow) * data.getCfl();
@@ -152,7 +128,7 @@ public class OneDimensionalModel implements Model {
     valveOpening = new double[maxStep + 1];
 
     // Zeitabhaengige Ergebnisse protokollieren
-    Q = new double[maxStep + 1];
+    inflow = new double[maxStep + 1];
     hMean = new double[maxStep + 1];
     I = new double[maxStep + 1];
     F0 = new double[maxStep + 1];
@@ -164,33 +140,34 @@ public class OneDimensionalModel implements Model {
     // Anfangsbedingungen der Felder setzen
     // Zellwerte
     for (int i = 0; i < nx; i++) {
-      A_schiff_cell[i] = data.getShipArea(dx * (i + 0.5));
+      aShipCell[i] = data.getShipArea(dx * (i + 0.5));
 
-      A00[i] = uw * kB - A_schiff_cell[i];
+      A00[i] = uw * kB - aShipCell[i];
       A0[i] = A00[i];
       A05[i] = 0.;
       A1[i] = A0[i];
-      
+
       beta[i] = 1.;
-      zufluss[i] = 0.;
+      massSource[i] = 0.;
+      momentumSource[i] = 0;
       h1[i] = 0.;
     }
 
     // Knotenwerte
     for (int i = 0; i < nx + 1; i++) {
-      A_schiff_node[i] = data.getShipArea(dx * i);
+      aShipNode[i] = data.getShipArea(dx * i);
       Q00[i] = 0.;
       Q0[i] = 0.;
       Q05[i] = 0.;
       Q1[i] = 0.;
-      
+
       v1[i] = 0;
     }
-    
+
     // Ergebnisse ueber die Zeit loeschen
     for (int i = 0; i < maxStep; i++) {
       hMean[i] = uw;
-      Q[i] = 0;
+      inflow[i] = 0;
       I[i] = (h1[0] - h1[nx - 2]) / (kL - dx);
       F0[i] = 0;
       F1[i] = 0;
@@ -204,7 +181,7 @@ public class OneDimensionalModel implements Model {
     runtime = System.nanoTime();
 
     // Ende der Füllung: min_dh
-    final double ow_uw = data.getDeltaWaterDepthStop();
+    final double ow_uw = data.getDeltaWaterDepthStop();    
 
     do {
 
@@ -214,34 +191,20 @@ public class OneDimensionalModel implements Model {
 
       timeSeries[step] = time;
 
-      final double aMue = filling.getAreaTimesLoss(time);
-
-      if (filling instanceof SegmentGateFillingType) {
-        valveOpening[step] = ((SegmentGateFillingType) filling)
-            .getSegmentGateAngle(time);
-      } else if (filling instanceof SluiceGateFillingType) {
-        valveOpening[step] = ((SluiceGateFillingType) filling)
-            .getSluiceGateHeight(time);
-      } else {
-        throw new IllegalArgumentException(
-            Messages.getString("errUnknownFillingType") //$NON-NLS-1$
-                + data.getFillingType());
+      if (filling instanceof GateFillingType) {
+        valveOpening[step] = ((GateFillingType) filling).getGateOpening(time);
       }
 
-      // Effektive Fallhöhe: Entweder OW bis Schütz oder OW bis UW
-      final double dh = Math.min(ow - h1[0], maxDh);
+      for (int i = 0; i < nx; i++) {
 
-      Q[step] = aMue * Math.sqrt(2. * GRAVITY * Math.abs(dh)
-          / (1. + zetaKanal / aKanal / aKanal * aMue * aMue));
+        final double[] source = filling.getSource(i * dx, time, h1[i], v1[i],
+            data);
 
-      // TODO: Warum eigentlich?
-      // Negative Qs abfangen
-      if (dh < 0.) {
-        Q[step] = 0.;
+        massSource[i] = source[0];
+        momentumSource[i] = source[1];
+
+        inflow[step] += massSource[i];
       }
-
-      // Vorkopffuellung: Zufluss am nullten Knoten, mit Zeitwichtung:
-      zufluss[0] = Q[step];
 
       // Alte Zeitebene wird ganz alte Zeitebene, neue Zeitebene wird alte
       // Zeitebene:
@@ -270,12 +233,19 @@ public class OneDimensionalModel implements Model {
         // = integral(U*U)dA / (integral(U)dA*integral(U)dA / A)
         for (int i = 0; i < nx; i++) {
 
-          // Strahlausbreitung
-          double aStrahl = aKanal
-              + Math.pow(aKanal, jetExponent) * (dx * i) * jetCoefficient;
-          aStrahl = Math.min(aStrahl, A05[i]);
+          if (filling instanceof GateFillingType) {
 
-          beta[i] = A05[i] / aStrahl;
+            // Strahlausbreitung
+            double aStrahl = ((GateFillingType) filling)
+                .getJetCrossSection(dx * i, time);
+            aStrahl = Math.min(aStrahl, A05[i]);
+
+            beta[i] = A05[i] / aStrahl;
+
+          } else {
+
+            beta[i] = 1.;
+          }
         }
 
         // AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
@@ -291,7 +261,7 @@ public class OneDimensionalModel implements Model {
 
         // Quellterme fuer jedes Volumen
         for (int i = 0; i < nx; i++) {
-          A1[i] += dt * zufluss[i] / dx;
+          A1[i] += dt * massSource[i] / dx;
         }
 
         // Neues A05 mit variabler Zeitwichtung ermitteln
@@ -314,8 +284,8 @@ public class OneDimensionalModel implements Model {
           final double rhy = 0.5 * (A05[i] + A05[i - 1]) / (3. * kB);
 
           // Wasserspiegelgefaelle mit Schiff:
-          final double gradH = ((A1[i] + A_schiff_cell[i]) / kB
-              - (A1[i - 1] + A_schiff_cell[i - 1]) / kB) / dx;
+          final double gradH = ((A1[i] + aShipCell[i]) / kB
+              - (A1[i - 1] + aShipCell[i - 1]) / kB) / dx;
           // Wasserspiegelgefaelle ohne Schiff:
           // double gradH=( A1[i]/KB -A1[i-1]/KB)/dx;
 
@@ -341,12 +311,15 @@ public class OneDimensionalModel implements Model {
                   / dx;
             }
           }
+
+          Q1[i] += momentumSource[i];
         }
         // Q fuer RB-Knoten
         // Q1[0] = 0.; // Impulsfreies Einleiten
-        Q1[0] = 1. * zufluss[0]; // Einleiten mit Impuls am Knoten 0 mit
-        // Faktor fuer die Richtung
-        Q1[nx] = 0.;
+
+        // Q1[0] = 1. * zufluss[0]; // Einleiten mit Impuls am Knoten 0 mit
+        // // Faktor fuer die Richtung
+        // Q1[nx] = 0.;
         // QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
 
         // Neues Q05 mit variabler Zeitwichtung ermitteln:
@@ -357,7 +330,7 @@ public class OneDimensionalModel implements Model {
 
       // Übertragen auf h und v: Nur fuer Postprocessing!
       for (int i = 0; i < nx; i++) {
-        h1[i] = (A1[i] + A_schiff_cell[i]) / kB;
+        h1[i] = (A1[i] + aShipCell[i]) / kB;
       }
 
       for (int i = 1; i < nx; i++) {
@@ -377,13 +350,13 @@ public class OneDimensionalModel implements Model {
       }
 
       // Volumenbilanz
-      vol += dt * zufluss[0];
+      chamberVol += dt * inflow[step];
 
       // Kraft auf Schiff [kN]
       for (int i = 0; i < nx - 1; i++) {
 
-        final double AA = A_schiff_node[i + 1];
-        final double AB = A_schiff_node[i];
+        final double AA = aShipNode[i + 1];
+        final double AB = aShipNode[i];
 
         // Massenkraft des Schiffes:
         F0[step] += 1000. * GRAVITY * AA * dx;
@@ -412,53 +385,54 @@ public class OneDimensionalModel implements Model {
       // < nx ; i ++) {System.out.println(i+" "+A1[i]+" "+Q1[i]+"
       // "+h1[i]+" "+A_schiff_cell[i]);}}
 
-    } while ((step < maxStep) && (hMean[step] < ow - ow_uw));    
-    
+    } while ((step < maxStep) && (hMean[step] < ow - ow_uw));
+
     runtime = System.nanoTime() - runtime;
   }
 
   private Results postprocess() {
 
-    double Qmax = 0., Imax = Double.MIN_VALUE, Imin = Double.MAX_VALUE, 
-        Fmax = Double.MIN_VALUE, Fmin = Double.MAX_VALUE,
-        schiff = 0., dQ_dt_min = Double.MAX_VALUE, 
-        dQ_dt_max = Double.MIN_VALUE;
+    double Qmax = 0., Imax = Double.MIN_VALUE, Imin = Double.MAX_VALUE,
+        Fmax = Double.MIN_VALUE, Fmin = Double.MAX_VALUE, shipVol = 0.,
+        dQ_dt_min = Double.MAX_VALUE, dQ_dt_max = Double.MIN_VALUE;
 
     for (int i = 0; i < step; i++) {
-      Qmax = Math.max(Qmax, Q[i]);
+      Qmax = Math.max(Qmax, inflow[i]);
       Imax = Math.max(Imax, I[i]);
       Imin = Math.min(Imin, I[i]);
       Fmax = Math.max(Fmax, F2[i]);
       Fmin = Math.min(Fmin, F2[i]);
       if (i > 0) {
-        if ((Q[i] - Q[i - 1]) / dt < dQ_dt_min) {
-          dQ_dt_min = (Q[i] - Q[i - 1]) / dt;
+        if ((inflow[i] - inflow[i - 1]) / dt < dQ_dt_min) {
+          dQ_dt_min = (inflow[i] - inflow[i - 1]) / dt;
         }
-        if ((Q[i] - Q[i - 1]) / dt > dQ_dt_max) {
-          dQ_dt_max = (Q[i] - Q[i - 1]) / dt;
+        if ((inflow[i] - inflow[i - 1]) / dt > dQ_dt_max) {
+          dQ_dt_max = (inflow[i] - inflow[i - 1]) / dt;
         }
       }
     }
 
     // Schiffsverdraengung [m^3]
     for (int i = 0; i < nx; i++) {
-      schiff += data.getShipArea(dx * i) * dx;
+      shipVol += data.getShipArea(dx * i) * dx;
     }
-    
-    System.out.println("*******************************************************"); //$NON-NLS-1$
+
+    System.out
+        .println("*******************************************************"); //$NON-NLS-1$
     System.out.printf("Zeitschritte: %d \n", step); //$NON-NLS-1$
     System.out.printf("Füllzeit: %f s \n", time); //$NON-NLS-1$
-    System.out.printf("Füllvolumen: %f m³/s \n", vol); //$NON-NLS-1$
-    System.out.printf("Schiffsvolumen: %f m³/s \n", schiff); //$NON-NLS-1$
+    System.out.printf("Füllvolumen: %f m³/s \n", chamberVol); //$NON-NLS-1$
+    System.out.printf("Schiffsvolumen: %f m³/s \n", shipVol); //$NON-NLS-1$
     System.out.printf("Qmax: %f m³/s \n", Qmax); //$NON-NLS-1$
     System.out.printf("Fx_min: %f kN  Fx_max: %f kN \n", Fmin, Fmax); //$NON-NLS-1$
     System.out.printf("Fx/G: %f  \n", //$NON-NLS-1$
-        Math.max(Fmax, Math.abs(Fmin)) / schiff / GRAVITY * 1000.);
+        Math.max(Fmax, Math.abs(Fmin)) / shipVol / GRAVITY * 1000.);
     System.out.printf("Imin: %f ‰  Imax: %f ‰ \n", //$NON-NLS-1$
         Imin * 1000., Imax * 1000.);
     System.out.printf("dQ/dt_min: %f m³/s²  dQ/dt_max: %f m³/s² \n", //$NON-NLS-1$
-            dQ_dt_min, dQ_dt_max);
-    System.out.println("*******************************************************"); //$NON-NLS-1$
+        dQ_dt_min, dQ_dt_max);
+    System.out
+        .println("*******************************************************"); //$NON-NLS-1$
     System.out.printf(Messages.getString("resultTotalRuntime"), //$NON-NLS-1$
         runtime * 1.e-9);
 
@@ -476,7 +450,7 @@ public class OneDimensionalModel implements Model {
 
       @Override
       public double[] getDischargeOverTime() {
-        return Q;
+        return inflow;
       }
 
       @Override
