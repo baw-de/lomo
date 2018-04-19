ef2908951e93176c640b9e1565ad4c677e3f1a50
Franz Simons <franz.simons@baw.de>
2018-04-19 11:16:44 +0200

    Added license headers and files.

--- src/main/java/de/baw/lomo/core/Messages.java
+++ src/main/java/de/baw/lomo/core/Messages.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ * Copyright (C) 2018 Bundesanstalt für Wasserbau
+ * 
+ * This file is part of LoMo.
+ * 
+ * LoMo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * LoMo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with LoMo.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 package de.baw.lomo.core;
 
 import java.util.MissingResourceException;
