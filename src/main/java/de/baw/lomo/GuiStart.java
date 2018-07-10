baw-de/lomo

commit 61adb588bac7b9e8bb87a269930ad0090616accb

It is now possible to give a case file name as argument when executing gui.

src/main/java/de/baw/lomo/GuiStart.java
@@ -17,8 +17,12 @@
  ******************************************************************************/
 package de.baw.lomo;
 
+import java.io.File;
+
 import de.baw.lomo.core.OneDimensionalModel;
+import de.baw.lomo.core.data.Case;
 import de.baw.lomo.gui.Controller;
+import de.baw.lomo.io.IOUtils;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -36,8 +40,16 @@ public class GuiStart extends Application {
         de.baw.lomo.gui.Messages.RESOURCE_BUNDLE);
     loader.load();  
       
-    Controller c = loader.getController();    
-    c.initModel(new OneDimensionalModel());
+    Controller c = loader.getController();  
+    Case data;
+    
+    try {
+      data = IOUtils.readCaseFromXml(new File(getParameters().getRaw().get(0)));
+    } catch (Exception e) {
+      data = null;
+    }    
+    
+    c.initModel(new OneDimensionalModel(),data);
     
     Parent root = loader.getRoot();    
     Scene scene = new Scene(root);

src/main/java/de/baw/lomo/gui/Controller.java
@@ -269,10 +269,15 @@ public class Controller implements Initializable {
     }
   }
 
-  public void initModel(Model model) {
+  public void initModel(Model model, Case data) {
 
     this.model = model;
-    this.data = new Case();
+    
+    if (data == null) {
+      this.data = new Case();
+    } else {
+      this.data = data;
+    }
 
     model.setCaseData(data);
 

commit 6aced174fbaad77101ab65308cbd908ff64509e7

Bug introduced in 61adb58. Default case was not initialized correctly.

src/main/java/de/baw/lomo/GuiStart.java
@@ -46,7 +46,7 @@ public class GuiStart extends Application {
     try {
       data = IOUtils.readCaseFromXml(new File(getParameters().getRaw().get(0)));
     } catch (Exception e) {
-      data = null;
+      data = new Case();
     }    
     
     c.initModel(new OneDimensionalModel(),data);